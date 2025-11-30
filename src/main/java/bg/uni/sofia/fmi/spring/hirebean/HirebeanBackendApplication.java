2afd859edfe10ad8d71ff2cd5f1d9f92871102f6
Stefan Shivarov Jr. <71455791+StefanShivarov@users.noreply.github.com>
2025-11-30 19:52:07 +0200

    Initialize application (#1)

---
+++ src/main/java/bg/uni/sofia/fmi/spring/hirebean/HirebeanBackendApplication.java
@@ -0,0 +1,13 @@
+package bg.uni.sofia.fmi.spring.hirebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirebeanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HirebeanBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/uni/sofia/fmi/spring/hirebean/HirebeanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.uni.sofia.fmi.spring.hirebean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirebeanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
